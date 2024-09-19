f7096e14b8cf012836130a7d3cbe86e185de04e0
promonkeyli <liyang_email@163.com>
2024-09-19 16:57:11 +0800

    feat(api): create api type login update

--- src/utils/create_api_type.go
+++ src/utils/create_api_type.go
@@ -2,28 +2,73 @@ package utils
 
 import (
 	"example.com/m/v2/src/public"
+	"fmt"
 	"github.com/getkin/kin-openapi/openapi3"
+	"strings"
 )
 
 type ApiTypeData struct {
-	ControllerName string
-	FileName       string
+	Name        string
+	Type        string
+	Description string
+	Properties  []Property
+}
+type Property struct {
+	Name        string
+	Description string
+	Type        string
 }
 
-func CreateApiType(o *openapi3.T, outDir string) {
-	// 处理 openAPI数据, 数组长度不固定，使用切片动态扩容
-	var data []ApiMainData
-	for _, tag := range o.Tags {
-		data = append(data, ApiMainData{
-			ControllerName: tag.Name,
-			FileName:       tag.Name,
-		})
+func CreateApiType(o openapi3.T, outDir string) {
+	// 1. 提取 schemas, swagger2中称为 definitions, openAPI3 更换为 components => schemas
+	schemas := o.Components.Schemas
+
+	var TplData []ApiTypeData
+	for key, schemaRef := range schemas {
+		name := strings.Split(key, ".")[1] // 类型名称
+		schema := *schemaRef.Value
+		typeArr := *schema.Type
+		properties := schema.Properties // 该类型的一些属性
+
+		var propertyArr []Property
+		for _, pSchemaRef := range properties {
+			pSchema := *pSchemaRef.Value
+			var pTypeArr []string
+			if *pSchema.Type == nil {
+				pTypeArr = []string{}
+			} else {
+				pTypeArr = *pSchema.Type
+			}
+
+			fmt.Println("=====", pTypeArr)
+
+			//var pType string
+			//if len(pTypeArr) > 0 {
+			//	pType = pTypeArr[0]
+			//} else {
+			//	pType = ""
+			//}
+			//propertyArrItem := Property{
+			//	Name:        pName,
+			//	Description: v.Description,
+			//	Type:        t[0],
+			//}
+			//propertyArr = append(propertyArr,[])
+		}
+
+		TplDataItem := ApiTypeData{
+			Name:        name,
+			Description: schema.Description,
+			Type:        typeArr[0],
+			Properties:  propertyArr,
+		}
+		TplData = append(TplData, TplDataItem)
 	}
 	// 模版配置数据组装
 	api := public.TplConfig{
 		TplPath:     "src/template/api_type.txt",
 		TplName:     "api_type.txt",
-		TplData:     data,
+		TplData:     TplData,
 		OutFileName: outDir + "/typings.d.ts",
 	}
 	RenderTpl(api)
